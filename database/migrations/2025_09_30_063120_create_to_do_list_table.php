20bdc728ae96cca3bc241f8a7f747c6dbc87227a
Fagian-01 <fagian030709@gmail.com>
2025-09-30 14:36:42 +0700

    update table

--- database/migrations/2025_09_30_063120_create_to_do_list_table.php
+++ database/migrations/2025_09_30_063120_create_to_do_list_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('description');
             $table->enum('status',['done','pending']);
+            $table->date('deadline')->nullable();
         });
     }
 
